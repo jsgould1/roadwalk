2356aa039a23d0ced5389611f98d5384a8f92a33
Gould <Jonathan.Gould@aecom.com>
2026-06-23 16:53:09 -0400

    Signs Report: swap logo order so USDOT seal is on the left, NPS arrowhead on the right
    Bump SW to v126.

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    ---------------------------------------------------------------- */
 'use strict';
 
-var CACHE_VERSION = 'roadwalk-v125';
+var CACHE_VERSION = 'roadwalk-v126';
 
 // Pre-cached on install so the app opens even on a fully cold start.
 var PRECACHE = ['./', './index.html', './roadwalk.html'];
